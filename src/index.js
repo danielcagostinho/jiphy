e6960d6b8b965ae327d65094999f530b6faf1b0a
danielcagostinho <danielcagostinho@gmail.com>
2022-03-12 17:51:37 -0500

    Added reference to base url in env variable

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename={process.env.REACT_APP_PUBLIC_URL}>
       <App />
       </Router>
     </Provider>
