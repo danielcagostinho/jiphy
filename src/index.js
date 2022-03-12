d9e9d789fdd7e0b3e4a09b0cd20630addb10f342
danielcagostinho <danielcagostinho@gmail.com>
2022-03-12 17:36:50 -0500

    Reverted Baseurl in index.html

--- src/index.js
+++ src/index.js
@@ -18,8 +18,7 @@ const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router  basename="/jiphy">
-
+      <Router>
       <App />
       </Router>
     </Provider>
