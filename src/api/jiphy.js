a9ab28bf00314a70c57140bf8846c59163baab8e
danielcagostinho <danielcagostinho@gmail.com>
2022-03-13 19:35:16 -0400

    Updates

--- src/api/jiphy.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-const instance = axios.create({
-  baseURL: 'https://api.giphy.com/v1/gifs/'
-});
-
-export default instance;
--- src/utils/Routes.js
+++
@@ -1,8 +0,0 @@
-export const routes = [
-  { displayName: "Main", address: "" },
-  { displayName: "About", address: "about" },
-  { displayName: "Privacy", address: "privacy" },
-  { displayName: "Terms of Sevice", address: "tos" },
-  { displayName: "Legal", address: "legal" },
-  { displayName: "Contact", address: "contact" },
-];
